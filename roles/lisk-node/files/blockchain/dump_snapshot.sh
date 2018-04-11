be9cb747310dde74f8d7e18fdb4d49b0afb10afd
Simon Warta <simon@kullo.net>
2018-04-12 00:12:34 +0200

    Add dump_snapshot.sh/restore_snapshot.sh

---
+++ roles/lisk-node/files/blockchain/dump_snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
+
+ORIGINAL_DB_NAME="lisk_test"
+SNAPSHOT_TIME="$(date --utc +%Y-%m-%dT%H:%M:%SZ)"
+SNAPSHOT_DB_NAME="${ORIGINAL_DB_NAME}_snapshot${SNAPSHOT_TIME}_pid$$"
+
+# Copy database into snapshot database to allow changes to the data
+echo "Copy data into snapshot database $SNAPSHOT_DB_NAME ..."
+createdb "$SNAPSHOT_DB_NAME"
+pg_dump "$ORIGINAL_DB_NAME" | psql "$SNAPSHOT_DB_NAME"
+
+echo "Deleting peers from snapshot data ..."
+psql -d "$SNAPSHOT_DB_NAME" -c 'delete from peers;'
+
+HEIGHT=$(psql -d "$SNAPSHOT_DB_NAME" -t -c 'SELECT height FROM blocks ORDER BY height DESC LIMIT 1;' | xargs)
+echo "Snapshot height is $HEIGHT"
+
+SNAPSHOT_FILE_NAME="${ORIGINAL_DB_NAME}_h${HEIGHT}_${SNAPSHOT_TIME}.pg_dump_dump"
+
+echo "Dumping snapshot to file $SNAPSHOT_FILE_NAME ..."
+pg_dump -Fcustom "$SNAPSHOT_DB_NAME" -f "$SNAPSHOT_FILE_NAME"
+
+echo "Removing snapshot database ..."
+dropdb "$SNAPSHOT_DB_NAME"
---
+++ roles/lisk-node/files/blockchain/restore_snapshot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
+
+SNAPSHOT_PATH="$1"
+
+TARGET_DB_NAME="lisk_test"
+
+CORES=$(sysctl -n hw.ncpu 2> /dev/null || nproc 2> /dev/null || echo 1)
+echo "Detected $CORES cores"
+
+JOBS=$(( CORES * 2 ))
+echo "Using $JOBS parallel restore jobs"
+
+echo "Removing current database $TARGET_DB_NAME if it exists ..."
+dropdb --if-exists "$TARGET_DB_NAME"
+
+echo "Creating empty database $TARGET_DB_NAME ..."
+createdb "$TARGET_DB_NAME"
+
+echo "Restoring snapshot ..."
+# Use --schema=public to avoid
+# could not execute query: ERROR:  must be owner of extension plpgsql
+# Command was: COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+pg_restore -Fcustom --schema=public -j "$JOBS" -d "$TARGET_DB_NAME" "$SNAPSHOT_PATH"
