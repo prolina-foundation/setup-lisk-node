831063dce2061eb2a853ffa9ce7b9c7e02ec4b0c
Simon Warta <simon@kullo.net>
2018-04-12 10:17:18 +0200

    Update SNAPSHOT_TIME format

--- roles/lisk-node/files/blockchain/dump_snapshot.sh
+++ roles/lisk-node/files/blockchain/dump_snapshot.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
 
 ORIGINAL_DB_NAME="lisk_test"
-SNAPSHOT_TIME="$(date --utc +%Y-%m-%dT%H:%M:%SZ)"
+SNAPSHOT_TIME="$(date --utc +%Y-%m-%dT%H-%M-%SZ)"
 SNAPSHOT_DB_NAME="${ORIGINAL_DB_NAME}_snapshot${SNAPSHOT_TIME}_pid$$"
 
 # Copy database into snapshot database to allow changes to the data
