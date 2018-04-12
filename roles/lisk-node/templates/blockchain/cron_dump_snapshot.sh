9304e82fd86eb0cb904d622ebdc349319600d642
Simon Warta <simon@kullo.net>
2018-04-12 14:07:22 +0200

    Add cron_dump_snapshot.sh

---
+++ roles/lisk-node/templates/blockchain/cron_dump_snapshot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+which shellcheck > /dev/null && (shellcheck -x "$0" || shellcheck "$0")
+
+DIR=$(mktemp -d)
+(
+    echo "Using temp dir $DIR ..."
+    cd "$DIR"
+    /workspace/dump_snapshot.sh {{ blockchain_database_name }}
+    FILES=(*.pg_dump_dump); FILE="${FILES[0]}"; sha256sum "$FILE" > "$FILE.sha256"
+    mv ./* /workspace/snapshots
+)
+
+echo "Removing temp dir $DIR ..."
+rm -r "$DIR"
